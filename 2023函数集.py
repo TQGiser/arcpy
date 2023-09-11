ee1be82761da26309975dde6b109707b70c245be
Administrator <540484369@qq.com>
2023-09-11 17:24:15 +0800

    修改两个函数

--- 2023函数集.py
+++ 2023函数集.py
@@ -11,9 +11,9 @@ def query_l_legnth(poline_file):
     for l in ls:
         print l.length,l.pointCount
 
-def extract_by_part(shpFile,shpName,ext_path):
+def extract_by_part_emptyFields(shpFile,shpName,ext_path):
     """
-    将包含多个要素的shp文件释放为单个的文件
+    将包含多个要素的shp文件释放为单个的文件，没有属性字段
     :param shpFile: 总的shp文件
     :param shpName: 提取出的文件命名前辍like:XX村_001
     :param ext_path: 释放的地址
@@ -26,4 +26,25 @@ def extract_by_part(shpFile,shpName,ext_path):
         aL = arcpy.CreateFeatureclass_management(ext_path, '{}{:03}.shp'.format(shpName,i), 'POLYGON')
         cs = arcpy.da.InsertCursor(aL, ['SHAPE@'])
         cs.insertRow([pl])
+        i+=1
+
+def extract_by_part_withFields(shpFile,ext_path):
+    """
+    将包含多个要素的shp文件释放为单个的文件,包含属性字段
+    :param shpFile: 总的shp文件
+    :param shpName: 提取出的文件命名前辍like:XX村_001
+    :param ext_path: 释放的地址
+    :return:
+    """
+    fileName = arcpy.Describe(shpFile).baseName.encode('utf-8')
+    cs = arcpy.da.SearchCursor(shpFile,['*'])
+    shpDatasList = [row for row in cs]
+    i=1
+    for data in shpDatasList:
+        aL = arcpy.CreateFeatureclass_management(
+            ext_path,
+            '{}{:03}.shp'.format(fileName,i),
+            template=shpFile)
+        cs = arcpy.da.InsertCursor(aL, ['*'])
+        cs.insertRow(data)
         i+=1
