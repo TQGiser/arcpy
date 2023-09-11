d61b4291a2c839712fdc7750f9677dde6dc0136e
Administrator <540484369@qq.com>
2023-09-11 16:41:52 +0800

    t

--- 草稿箱/0908.py
+++ 草稿箱/0908.py
@@ -11,25 +11,7 @@ tp2 = r'E:\workData\20230908XXD\测试\84\土壤类型84.shp'
 hbs = F.listFiles(path,'','m')
 i=0
 for hb in hbs:
-
     ofp = repath + '\\' + '土壤类型{:02}.shp'.format(i)
     print ofp
     arcpy.Clip_analysis(tp2,hb,ofp,cluster_tolerance="")
     i+=1
-
-
-
-
-
-
-
-
-
-
-# for boua in bouas:
-#     print boua
-#     with arcpy.da.SearchCursor(boua,'SHAPE@') as yb2:
-#         for row2 in yb2:
-#             cs.insertRow(row2)
-# Shp = arcpy.Dissolve_management(aL,path + '\\' + 'DMAA.shp')
-# arcpy.Delete_management(aL)
